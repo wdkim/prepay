dabbbe4650273c8775cf2aa884fa68141308f436
Michael Mitchell <mitch@gatech.edu>
2013-03-24 12:34:10 -0400

    Added browse listings by category

--- prepay/models.py
+++ prepay/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+from decimal import Decimal
 
+'''
+#http://stackoverflow.com/questions/2013835/django-how-should-i-store-a-money-value
+class CurrencyField(models.DecimalField):
+  __metaclass__ = models.SubfieldBase
+
+  def __init__(self, verbose_name=None, name=None, **kwargs):
+    super(CurrencyField, self). __init__(
+        verbose_name=verbose_name, name=name, max_digits=10,
+        decimal_places=2, **kwargs)
+
+  def to_python(self, value):
+    try:
+      return super(CurrencyField, self).to_python(value).quantize(Decimal("0.01"))
+    except AttributeError:
+      return None
+'''
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -50,6 +67,8 @@ class Listing(models.Model):
     seller = models.ForeignKey(Seller)
     product = models.ForeignKey(Product)
     description = models.TextField(max_length=1000)
+    #quantity = models.IntegerField()
+    #price_per = CurrencyField()
     
     created_at = models.DateTimeField(auto_now_add=True)
     
--- prepay/views.py
+++ prepay/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render, get_object_or_404
 
-from prepay.models import Listing
+from prepay.models import Listing, Category
 
 def index(request):
     return render(request, 'prepay/home.html')
@@ -10,15 +10,42 @@ def index(request):
 def about(request):
     return render(request, 'prepay/about.html')
 
+'''
+todo: 
+refactor this to support browse by different criteria e.g. category
+for now, created redundant browse_by_category
+'''
 def browse(request):
-
     all_listings = Listing.objects.all().order_by('-created_at')
-    #template = loader.get_template('myILS/browse.html')
     context = Context({
         'all_listings': all_listings,
     })
-    
     return render(request, 'prepay/browse.html', context)
+
+'''
+Pretty sick how this:
+
+select prepay_listing.name, prepay_product.name, prepay_category.name
+from prepay_product, prepay_category, prepay_product_categories, prepay_listing
+where prepay_product.id = prepay_product_categories.product_id
+and prepay_product.id = prepay_listing.product_id
+and prepay_category.id = prepay_product_categories.category_id
+and prepay_category.id = 1
+
+equals this:
+
+Listing.objects.filter(product__category__exact=cat_id)
+
+'''
+def browse_category(request, category_id):
+    category = Category.objects.filter(pk = category_id)
+    listings_by_category = Listing.objects.filter(product__categories__exact=category_id)
+    print 'category? ' + category[0].name
+    context = Context({
+        'category': category[0],
+        'listings_by_category': listings_by_category,
+    })
+    return render(request, 'prepay/browse_category.html', context)
 
 def listing_detail(request, listing_id):
     #return HttpResponse("You're looking at the detailed view of listing %s." % listing_id)
