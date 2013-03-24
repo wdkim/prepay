a3437a7e5d0687ab906315dc73585f8ce01d930a
jlin45 <mmkkna@googlemail.com>
2013-03-24 14:32:40 -0300

    Added url for register view

--- prepay/urls.py
+++ prepay/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^about$', views.about, name='about'),
     url(r'^browse_category/(?P<category_id>.*)$', views.browse_category, name='browse_category'),
     url(r'^browse$', views.browse, name='browse'),
+    url(r'^register$', views.register, name = 'register'), ###Jennifer
     url(r'^listings/(?P<listing_id>.*)$', views.listing_detail, name='listing_detail'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
